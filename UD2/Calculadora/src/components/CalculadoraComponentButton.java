f6476e2a982f1230239e433e02b0c9439611d10a
JoseRubicco21 <a23joserf@iessanclemente.net>
2025-10-06 12:55:20 +0200

    feat: :sparkles: Start  calculadora

---
+++ UD2/Calculadora/src/components/CalculadoraComponentButton.java
@@ -0,0 +1,13 @@
+package components;
+
+import javax.swing.JButton;
+
+public class CalculadoraComponentButton extends JButton{
+    
+
+    public CalculadoraComponentButton(String text) {
+        super(text);
+    }
+
+    
+}
---
+++ UD2/Calculadora/src/controller/CalculadoraController.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class CalculadoraController {
+
+
+    public double add(int a, int b) {
+        return a + b;
+    }
+
+    public double subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+
+    public double power(int a, int b) {
+        return Math.pow(a, b);
+    }
+
+}
